e43983b41a54f44baa15db463d2c04ecbdef9ca5
Milton-Carlos-Ribeiro <165504644+Milton-Carlos-Ribeiro@users.noreply.github.com>
2024-04-14 22:20:48 +0900

    Saved curriculum

---
+++ dados.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var profileImage = document.getElementById("profile-image");
+
+    // Substitua o valor de 'src' pelo caminho correto da sua imagem.
+    profileImage.src = "\Assets\Milton Carlos.png";
+});
