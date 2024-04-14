72bed112f60b7a8da4ccd9da249974879c69071d
Milton-Carlos-Ribeiro <165504644+Milton-Carlos-Ribeiro@users.noreply.github.com>
2024-04-14 22:34:21 +0900

    Delete dados.js

--- dados.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var profileImage = document.getElementById("profile-image");
-
-    // Substitua o valor de 'src' pelo caminho correto da sua imagem.
-    profileImage.src = "\Assets\Milton Carlos.png";
-});
